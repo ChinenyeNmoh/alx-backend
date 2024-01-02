27c3e4c9c7514214724112eeb273dcb363ddca59
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2024-01-02 21:10:19 +0100

    cache

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -15,10 +15,11 @@ class LIFOCache(BaseCaching):
         """
         if key and item:
             self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            self.cache_data.popitem()
-            discarded_key, _ = self.cache_data.popitem()
-            print(f"DISCARD: {discarded_key}")
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                self.cache_data.popitem()
+                discarded_key, _ = self.cache_data.popitem()
+                print(f"DISCARD: {discarded_key}")
+                self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key
@@ -26,3 +27,17 @@ class LIFOCache(BaseCaching):
         if key and key in self.cache_data:
             return self.cache_data[key]
         return None
+my_cache = LIFOCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+my_cache.put("F", "Mission")
+my_cache.print_cache()
+my_cache.put("G", "San Francisco")
+my_cache.print_cache()
