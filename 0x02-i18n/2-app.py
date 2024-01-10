efcbca50e21bf339b74497de8619571e658b61cd
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2024-01-10 03:32:08 +0100

    translation

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python3
-"""app config"""
+""" flask application"""
+
 from flask import Flask, render_template, request
 from flask_babel import Babel
-
-
 app = Flask(__name__)
 babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale():
-    """ get_locale
-    """
-    return return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-class Config(object):
-    """Configure the app"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+class Config:
+    """Represents flask Babel configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale():
+    """return the best match from the supported
+    languages based on the request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/', strict_slashes=False)
-def home():
-    """Renders a Basic Template"""
+def index() -> str:
+    """welcome to flask application"""
     return render_template('2-index.html')
 
 
